e3dcc5af4df8912281b8ef1533a90bae768e1bd7
Fredrik Welander <welandfr@arcada.fi>
2022-03-23 11:01:48 +0200

    lektion 2 mellancommit

--- app/src/main/java/fi/arcada/sos22_exempel/MainActivity.java
+++ app/src/main/java/fi/arcada/sos22_exempel/MainActivity.java
@@ -3,12 +3,31 @@ package fi.arcada.sos22_exempel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // deklarera variabler
+    TextView textHello;
+    EditText editTextName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialisera variabler
+        textHello = findViewById(R.id.helloText);
+        editTextName = findViewById(R.id.editTextName);
+
+        //textHello.setText("Hello from MainActivity!");
     }
+
+    public void btnClick(View view) {
+        //String name = editTextName.getText().toString();
+        textHello.setText("Hello " + editTextName.getText());
+    }
+
 }
