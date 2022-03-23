2ffa87e87c41ba90bcfaaff1cbd4a0cc3e97ac09
Fredrik Welander <welandfr@arcada.fi>
2022-03-23 11:49:53 +0200

    lektion 2 add calcMean

--- app/src/main/java/fi/arcada/sos22_exempel/MainActivity.java
+++ app/src/main/java/fi/arcada/sos22_exempel/MainActivity.java
@@ -10,8 +10,10 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     // deklarera variabler
-    TextView textHello;
+    TextView textHello, textMean;
     EditText editTextName;
+
+    double[] testdata = { 3.0, 5.2, 6.0, 4.0, 2.0, 4.3, 5.0, 7.0, 8.0 };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Initialisera variabler
         textHello = findViewById(R.id.helloText);
+        textMean = findViewById(R.id.textViewMean);
         editTextName = findViewById(R.id.editTextName);
 
         //textHello.setText("Hello from MainActivity!");
@@ -27,7 +30,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void btnClick(View view) {
         //String name = editTextName.getText().toString();
-        textHello.setText("Hello " + editTextName.getText());
+        textHello.setText(
+            String.format("Hello %s the number is %d",
+                editTextName.getText(),
+                5
+            )
+        );
     }
 
+    public void calculate(View view) {
+
+        String meanStr = String.format("mean: %.2f",
+                Statistics.calcMean(testdata)
+        );
+
+        textMean.setText(meanStr);
+    }
 }
