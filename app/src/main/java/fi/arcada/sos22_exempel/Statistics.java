aa4c6eb8eb79deee3db03e839d970fd7cc17aa31
Fredrik Welander <fredrik.welander@subsite.fi>
2022-03-25 10:49:55 +0200

    Lektion 3 mellancommit

--- app/src/main/java/fi/arcada/sos22_exempel/Statistics.java
+++ app/src/main/java/fi/arcada/sos22_exempel/Statistics.java
@@ -1,14 +1,56 @@
 package fi.arcada.sos22_exempel;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Statistics {
 
+    // Sorteringsmetod att användas i andra metoder
+    public static ArrayList<Double> getSorted(ArrayList<Double> dataset) {
+        // Vi måste skapa en kopia av vår datamängd så vi inte sorterar den ursprungliga
+        ArrayList<Double> sorted = new ArrayList<>(dataset);
+        Collections.sort(sorted);
+        return sorted;
+    }
+
     // Statisk metod som kan köras utan att skapa ett klassobjekt
-    public static double calcMean(double[] dataset) {
+    public static double calcMean(ArrayList<Double> dataset) {
+
         double sum = 0;
-        for (int i = 0; i < dataset.length; i++) {
-            sum += dataset[i];
+        for (int i = 0; i < dataset.size(); i++) {
+            sum += dataset.get(i);
         }
 
-        return sum / dataset.length;
+        return sum / dataset.size();
+    }
+
+    // Median
+    public static double calcMedian(ArrayList<Double> dataset) {
+        ArrayList<Double> sorted = getSorted(dataset);
+        int mid = sorted.size() / 2;
+        double median;
+        if (sorted.size() % 2 == 0) {
+            // Om antalet är jämnt, ta medelvärdet av de två mittersta
+            median = sorted.get(mid-1) + sorted.get(mid) / 2;
+        } else {
+            // Om antalet är udda, ta det mittersta värdet
+            median = sorted.get(mid);
+        }
+        return median;
+    }
+
+    // Standardavvikelse (Standard Deviation
+    public static double calcSD(ArrayList<Double> dataset) {
+        double sumDiff = 0;
+        double avg = calcMean(dataset);
+
+        // Loopa igenom datamängden
+        for (double dataVal: dataset) {
+            // Summan av de enskilda skillnaderna i kvadrat
+            sumDiff += Math.pow(dataVal-avg,2);
+        }
+
+        return Math.sqrt(sumDiff / dataset.size());
+
     }
 }
--- app/src/main/sos22_exempel/MainActivity.java
+++ app/src/main/sos22_exempel/MainActivity.java
@@ -7,19 +7,14 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class MainActivity extends AppCompatActivity {
 
     // deklarera variabler
-    TextView textHello, textMean, textDataOut;
+    TextView textHello, textMean;
     EditText editTextName;
-    // Vi skapar en arraylist för vår datamängd
-    ArrayList<Double> dataset = new ArrayList<>();
 
     // vi skapar en array med lite data att testa med
-    double[] testdata = { 3.0, 5.2, 6.0, 4.0, 2.0, 4.3, 5.0, 7.0, 8.0, 7.3 };
+    double[] testdata = { 3.0, 5.2, 6.0, 4.0, 2.0, 4.3, 5.0, 7.0, 8.0 };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +24,7 @@ public class MainActivity extends AppCompatActivity {
         // Initialisera variabler
         textHello = findViewById(R.id.helloText);
         textMean = findViewById(R.id.textViewMean);
-        textDataOut = findViewById(R.id.datasetOut);
         editTextName = findViewById(R.id.editTextName);
-
-        for (int i = 0; i < testdata.length; i++) {
-            // Vi lägger till testdata i vår ArrayList
-            dataset.add(testdata[i]);
-        }
-
-        // Vi skriver tillfälligt ut vår datamängd
-        String dataOut = "";
-        for (double number: dataset) {
-            dataOut += number + " ";
-        }
-        textDataOut.setText(dataOut);
 
     }
 
@@ -59,10 +41,8 @@ public class MainActivity extends AppCompatActivity {
     public void calculate(View view) {
 
         // %.2f i String.format() avrundar till två decimaler
-        String meanStr = String.format("Medelvärde: %.2f\n Median: %.2f\nStd.avvikelse: %.2f",
-                Statistics.calcMean(dataset),
-                Statistics.calcMedian(dataset),
-                Statistics.calcSD(dataset)
+        String meanStr = String.format("mean: %.2f",
+                Statistics.calcMean(testdata)
         );
 
         textMean.setText(meanStr);
